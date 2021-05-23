d58b4a495196c7ca5f852205391cf28c5b172b24
Matthew Lin <mlin2814@gmail.com>
2021-05-23 16:18:54 -0400

    Updated with clean code and corrected information

--- script.js
+++ script.js
@@ -6,32 +6,3 @@ function collapse() {
         nav.className = "nav-bar";
     }
 }
-
-// document.addEventListener('DOMContentLoaded', sendForm);
-
-// function sendForm() {
-//     document.getElementById('submit').addEventListener('click', function(event) {
-//         let req = new XMLHttpRequest();
-//         let payload = {name:null, email:null, comment:null}
-
-//         payload.name = document.getElementById('name').value 
-//         payload.email = document.getElementById('email').value 
-//         payload.comment = document.getElementById('comment').value 
-
-//         console.log(payload)
-
-//         req.open('POST', 'http://web.engr.oregonstate.edu/~zhangluy/tools/class-content/form_tests/check_request.php')
-//         req.setRequestHeader('Content-Type', 'application/json')
-//         req.addEventListener('load', function() {
-//             if(req.status >= 200 && req.status < 400){
-//                 var response = JSON.parse(req.responseText);
-//                 document.getElementById('enteredText').textContent = payload;
-//                 console.log(response)
-//               } else {
-//                 console.log("Error in network request: " + req.statusText);
-//               }});
-//             req.send(JSON.stringify(payload));
-//             event.preventDefault();
-//         // })
-//     })
-// }
